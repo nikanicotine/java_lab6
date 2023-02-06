b855505bf6a637e13b95c679c4f6015cb3111ce3
y.gorozhanin <y.gorozhanin@setupit.org>
2023-02-06 22:39:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Dialog MainWindow = new Dialog();
+        MainWindow.pack();
+        MainWindow.setSize(800,800);
+        MainWindow.setTitle("Laba1");
+        MainWindow.setVisible(true);
+    }
+}
